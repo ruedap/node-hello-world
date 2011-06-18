a780d41dd333063cdf4e074d4e31e75fadedb83d
ruedap <rueda.jp@gmail.com>
2011-06-18 23:46:59 +0900

    first commit

---
+++ web.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.get('/', function(req, res) {
+    res.send('HerokuでNode.jsとExpressを使ってHello world!');
+}
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
